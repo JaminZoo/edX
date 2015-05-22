89065149af0690d56b0de4d2d90a20cbbe4406d0
JaminZoo <b.zhu86@gmail.com>
2015-05-23 00:49:29 +1000

    Framingham study notes

--- Unit 3 - Logistic Regression/Unit3.R
+++ Unit 3 - Logistic Regression/Unit3.R
@@ -10,7 +10,9 @@
 #the probability of 1 i.e. improved care. 
 
 # exp(x) takes the exponent of x
+
 ****************************************
+# Care quality for patients with diabeties 
 quality = read.csv("quality.csv")
 # Install package to randomnise data frame into Train and Test groups
 install.packages("caTools")
@@ -49,3 +51,40 @@ plot(ROCperf, colorize= T, print.cutoffs.at = seq(0,1,0.1), text.adj = c(-0.2,1.
 # colors plot and adds 0.1 threshold data points
 # Determine AUC (area under chart) for predictTest 
 auc = as.numeric(performance(ROCpredicTest, "auc")@y.values)
+
+***********************************
+# Framingham hear study: evaluating risk factors to predict Cornary Heart Disease (CHD) in patients 
+#TenYearCHD is the dependent variable of interest, and the risk factors of CHD are assigned as ind. var
+  
+fram = read.csv("framingham.csv")
+# Set random seed in accordance with video tutorial
+set.seed(1000)
+
+# split fram data fram into train and test data frames. Typically want to put 50 - 80% of your 
+# data in the train set vs. test set (a larger main data set, the lower the split ratio can be)
+split = sample.split(fram$TenYearCHD, SplitRatio = 0.65)
+
+# Create Train and Test subsets based on the true/false vaues of split
+framTrain = subset(fram, split == T)
+framTest = subset(fram, split == F)
+
+# Create logistic regression model using the train data set ( "." means use all variables in data set as independent)
+framLog = glm(TenYearCHD ~ ., data = framTrain, family = binomial)
+# Create a prediction based on the test data frame and the logistic model just created
+predicTest = predict(framLog, type = 'response', newdata = framTest)
+
+# Create a confusion (classification) matrix to compare actual (Test) ad predicted (framLog) outcomes
+table(framTest$TenYearCHD, predicTest > 0.5)
+# results show that our model rarely predicts a ten year CHD risk above 50%
+# Prediction accuracy i.e. of predicting a true outcome can be calculated by taking the 
+# total true predictions (for both T & F) and dividing by total observations
+predicAccur = (1069 + 11)/(1069 + 11 + 6 + 187)
+# Use RORC to make predictions based on predicTest and test data set
+ROCRpredic = prediction(predicTest, framTest$TenYearCHD)
+# Calculate the Area under chart 
+auc = as.numeric(performance(ROCRpredic, "auc")@y.values)
+
+# External validation with other sets of data (not Framingham) is critical to validate the train model
+#accuracy when predicting CHD risk for other ethnicities e.g. Black, Asian, Hispanic etc.
+
+
